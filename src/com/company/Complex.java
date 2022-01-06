6a9e98f55da610fe173b91d9d3de68fb3b709e4d
fdsa <fdsa>
2022-01-06 12:36:21 -0500

    fsda

--- src/com/company/Complex.java
+++ src/com/company/Complex.java
@@ -18,20 +18,41 @@ public class Complex {
     }
 
     public Complex add(Complex other){
-        return new Complex((a + other.a), (b + other.b));
+        return new Complex(a + other.a, b + other.b);
+    }
+
+    public Complex add(double x){
+        return new Complex(a + x, b);
     }
 
     public String toString(){
         return a + " + " + b + " * i";
     }
 
+    public Complex multiply(Complex other){
+        return new Complex(a * other.a - b * other.b, a * other.b + b * other.a);
+    }
+
+    public Complex multiply(double x){
+        return new Complex(a * x, b * x);
+    }
+
     public static void main(String[] args) {
-        Complex c1 = new Complex(1);
-        Complex c2 = new Complex(1, 4);
-        Complex c3 = new Complex(2, 3);
-        Complex c4 = new Complex(5, 6);
-        Complex c5 = new Complex(0, 4);
-        Complex c6 = new Complex(0, 0);
+        Complex c1 = new Complex(1, 4);
+        Complex c2 = new Complex(2, 3);
+        double d = 4;
+
+        System.out.println(c1.abs());
+        System.out.println(c2.abs());
+
+        System.out.println(c1.add(c2));
+        System.out.println(c1.add(d));
+
+        System.out.println(c1.multiply(c2));
+        System.out.println(c1.multiply(d));
+
+        System.out.println(c1.toString());
+        System.out.println(c2.toString());
     }
 
 }
--- src/com/company/SoccerTeam.java
+++ src/com/company/SoccerTeam.java
@@ -1,4 +1,47 @@
 package com.company;
 
 public class SoccerTeam {
+    private int wins;
+    private int losses;
+    private int ties;
+
+    private static int goals;
+    private static int gamesPlayed;
+
+    public SoccerTeam(int w, int l, int t){
+        wins = w;
+        losses = l;
+        ties = t;
+    }
+
+    public void played(SoccerTeam other, int myScore, int otherScore){
+        gamesPlayed ++;
+        goals += myScore + otherScore;
+        if (myScore > otherScore){
+            wins ++;
+            other.losses ++;
+        }
+        else if (myScore < otherScore){
+            losses ++;
+            other.wins ++;
+        }
+        else{
+            ties ++;
+            other.ties ++;
+        }
+    }
+
+    public int teamPoints(){
+        return wins * 3 + ties;
+    }
+
+    public void reset(){
+        wins = 0;
+        losses = 0;
+        ties = 0;
+    }
+
+    public static void
+
+
 }
