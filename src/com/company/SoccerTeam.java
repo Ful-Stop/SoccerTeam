2d0e763dc42065433300f522e4ce168ef8cd02b2
fdsa <fdsa>
2022-01-11 11:35:20 -0500

    fdsa

--- src/com/company/SoccerTeam.java
+++ src/com/company/SoccerTeam.java
@@ -8,10 +8,10 @@ public class SoccerTeam {
     private static int goals;
     private static int gamesPlayed;
 
-    public SoccerTeam(int w, int l, int t){
-        wins = w;
-        losses = l;
-        ties = t;
+    public SoccerTeam(){
+        wins = 0;
+        losses = 0;
+        ties = 0;
     }
 
     public void played(SoccerTeam other, int myScore, int otherScore){
@@ -41,7 +41,81 @@ public class SoccerTeam {
         ties = 0;
     }
 
-    public static void
+    public static int getGoals(){
+        return goals;
+    }
+
+    public static int getGamesPlayed(){
+        return gamesPlayed;
+    }
+
+    public static void startTournament(){
+        goals = 0;
+        gamesPlayed = 0;
+    }
+
+    public static void main(String[] args) {
+        SoccerTeam billerica = new SoccerTeam();
+        SoccerTeam tewksbury = new SoccerTeam();
+        SoccerTeam methuen = new SoccerTeam();
+        SoccerTeam haverhill = new SoccerTeam();
+
+        billerica.played(tewksbury, 5, 2);
+        billerica.played(methuen, 8, 0);
+        billerica.played(haverhill, 2, 6);
+
+        tewksbury.played(billerica, 4, 7);
+        tewksbury.played(methuen, 5, 6);
+        tewksbury.played(haverhill, 0, 2);
+
+        methuen.played(billerica, 9, 4);
+        methuen.played(tewksbury, 5, 6);
+        methuen.played(haverhill, 4, 2);
+
+        haverhill.played(billerica, 0, 1);
+        haverhill.played(tewksbury, 9, 8);
+        haverhill.played(methuen, 5, 5);
+
+        System.out.println(billerica.teamPoints());
+        System.out.println(tewksbury.teamPoints());
+        System.out.println(methuen.teamPoints());
+        System.out.println(haverhill.teamPoints());
+
+        System.out.println(getGoals());
+        System.out.println(getGamesPlayed());
+
+        billerica.reset();
+        tewksbury.reset();
+        methuen.reset();
+        haverhill.reset();
+
+        startTournament();
+
+        billerica.played(tewksbury, 5, 2);
+        billerica.played(methuen, 1, 4);
+        billerica.played(haverhill, 3, 3);
+
+        tewksbury.played(billerica, 4, 7);
+        tewksbury.played(methuen, 8, 6);
+        tewksbury.played(haverhill, 7, 7);
+
+        methuen.played(billerica, 9, 4);
+        methuen.played(tewksbury, 5, 11);
+        methuen.played(haverhill, 2, 2);
+
+        haverhill.played(billerica, 0, 0);
+        haverhill.played(tewksbury, 5, 3);
+        haverhill.played(methuen, 1, 7);
+
+        System.out.println(billerica.teamPoints());
+        System.out.println(tewksbury.teamPoints());
+        System.out.println(methuen.teamPoints());
+        System.out.println(haverhill.teamPoints());
+
+        System.out.println(getGoals());
+        System.out.println(getGamesPlayed());
+
+    }
 
 
 }
